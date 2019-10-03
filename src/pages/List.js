5d2d2fed3f4a603912aca58071ec6e5b726e6e1d
kleyson castro <kleysoncastro99@gamil.com>
2019-10-03 10:29:05 -0300

    pagias criadas

---
+++ src/pages/List.js
@@ -0,0 +1,9 @@
+import react from 'react'
+import {Veew} from 'react-native'
+
+export default function List() {
+
+return <View />
+
+
+}// fim List
---
+++ src/pages/Login.js
@@ -0,0 +1,9 @@
+import react from 'react'
+import {Veew} from 'react-native'
+
+export default function Login() {
+
+return <View />
+
+
+}// fim login
