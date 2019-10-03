f74bea26e8844ba4cbac4049238f9ddabdcf0289
kleyson castro <kleysoncastro99@gamil.com>
2019-10-03 14:35:22 -0300

    Estilo de pagina feito

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,104 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {View, KeyboardAvoidingView, StyleSheet, Image, Text, TextInput, TouchableOpacity} from 'react-native'
+
+import logo from '../assets/logo.png'
 
 export default function Login() {
 
-return <View>
+return (
+        
+  <KeyboardAvoidingView styles={styles.container} behavior="padding" enabled > 
 
-    <Text>Hello word</Text>
+     <Image source={logo} style={styles.imageLogo}/> 
+    <View style={styles.form}>
+            <Text style={styles.label}>Seu E-mail *</Text>
 
-</View>
+            <TextInput 
+                style={styles.input}
+                placeholder="Seu email"
+                placeholderTextColor="#999"
+                keyboardType="email-address"
+                autoCapitalize="none"
+                autoCorrect={false} />
 
 
+
+        <Text style={styles.label}>Tecnologias *</Text>
+
+        <TextInput 
+            style={styles.input}
+            placeholder="Tecnologias de interesse"
+            placeholderTextColor="#999"
+            autoCapitalize="words"
+            autoCorrect={false} />
+
+            <TouchableOpacity style={styles.button}>
+
+                <Text style={styles.buttonText}>Encontrar spots</Text>
+            </TouchableOpacity>
+            </View>
+    
+            </KeyboardAvoidingView>
+
+
+    )
 }// fim login
+
+const styles =  StyleSheet.create({
+
+    container: {
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+           
+    },
+
+    form: {
+
+        alignSelf: 'stretch',
+        paddingHorizontal: 30,
+        marginTop: 30,
+    },
+
+    label: {
+        fontWeight: 'bold',
+        color: '#444',
+        marginBottom: 8,
+        
+    },
+
+    imageLogo: {
+
+        marginTop: 150,
+        marginHorizontal: 100
+    },
+
+    input: {
+        
+        borderWidth: 1,
+        borderColor: '#ddd',
+        paddingHorizontal: 20,
+        fontSize: 16,
+        color: '#444',
+        height: 44,
+        marginBottom: 20,
+        borderRadius: 4
+    },
+
+    button: {
+
+        height: 42,
+        backgroundColor: '#f05a5b',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 4
+    },
+
+    buttonText: {
+
+        color: '#fff',
+        fontWeight: 'bold',
+        fontSize: 16
+    }
+
+});
