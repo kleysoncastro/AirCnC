619abec01b2636a018b0af43c77eff975cd86b91
kleyson castro <kleysoncastro99@gamil.com>
2019-10-04 19:39:16 -0300

    finalizado

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,12 +1,29 @@
 import React, {useState, useEffect} from 'react'
-import {SafeAreaView, ScrollView, Text,StyleSheet, AsyncStorage, Image} from 'react-native'
-
+import {SafeAreaView, Alert, ScrollView, Text,StyleSheet, AsyncStorage, Image} from 'react-native'
+import sockeriocli from 'socket.io-client'
 import SpotList from '../components/SpotList'
 import logo from '../assets/logo.png'
 
 export default function List() {
 
     const [techs, setTechs] = useState([]);
+ 
+    useEffect(()=>{
+        AsyncStorage.getItem('user').then(user_id => {
+            const socket = sockeriocli('http://192.168.0.9:3333', {
+                query: {user_id}
+            })
+
+            socket.on('booking_response', booking => {
+
+                Alert.alert(`Sua reserva em ${booking.spot.company} em ${booking.date} 
+                foi ${booking.approved ? 'Aprovado': 'Negado'}`)
+
+            })
+        })
+
+
+    },[]);
 
 
     useEffect(()=>{
